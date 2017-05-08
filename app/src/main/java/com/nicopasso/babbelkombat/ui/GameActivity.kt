c2de444e2760e64baf51115409e826361db300ef
Niccolò Passolunghi <nicopasso@gmail.com>
2017-05-08 11:51:09 +0200

    Initialize the UI in GameActivity

--- app/src/main/java/com/nicopasso/babbelkombat/ui/GameActivity.kt
+++ app/src/main/java/com/nicopasso/babbelkombat/ui/GameActivity.kt
@@ -2,11 +2,15 @@ package com.nicopasso.babbelkombat.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.nicopasso.babbelkombat.R
 import com.nicopasso.babbelkombat.model.Game
 import com.nicopasso.babbelkombat.model.Player
 import com.nicopasso.babbelkombat.model.Word
+import com.nicopasso.babbelkombat.ui.StartActivity.Companion.TEXT_FONT
 import com.nicopasso.babbelkombat.utils.BKUtils
+import kotlinx.android.synthetic.main.activity_game.*
+import kotlinx.android.synthetic.main.player_layout.view.*
 import org.parceler.Parcels
 
 class GameActivity: AppCompatActivity() {
@@ -24,6 +28,7 @@ class GameActivity: AppCompatActivity() {
 
     var selectedWordIndex = 0
     var selectedWord: Word? = null
+    var roundNumber = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,6 +66,49 @@ class GameActivity: AppCompatActivity() {
                 gameWords.add(it)
             }
         }
+    }
+
+    fun initUI() {
+
+        round_textview.text = resources.getString(R.string.round_number, roundNumber)
+
+        word_to_guess_textview.text = selectedWord?.textEng
+
+        play_btn.apply {
+            applyFont(TEXT_FONT)
+            setOnClickListener {
+                round_container_layout.visibility = View.GONE
+                words_container_layout.visibility = View.VISIBLE
+                //TODO: start the game here
+            }
+        }
+
+        player_one.arcade_btn.tag = 1
+        player_two.arcade_btn.tag = 2
+        player_three.arcade_btn.tag = 3
+        player_four.arcade_btn.tag = 4
+
+        when(players.count()) {
+            1 -> {
+                player_two.visibility = View.GONE
+                player_three.visibility = View.GONE
+                player_four.visibility = View.GONE
+                player_one.player_name_textview.text = players[0].name
+            }
+            2 -> {
+                player_three.visibility = View.GONE
+                player_four.visibility = View.GONE
+                player_one.player_name_textview.text = players[0].name
+                player_two.player_name_textview.text = players[1].name
+            }
+            3 -> {
+                player_four.visibility = View.GONE
+                player_one.player_name_textview.text = players[0].name
+                player_two.player_name_textview.text = players[1].name
+                player_three.player_name_textview.text = players[2].name
+            }
+        }
+
     }
 
 
