ccf2518511f4b004d7b9a4e412d0d986a68e9df8
Niccolò Passolunghi <nicopasso@gmail.com>
2017-05-08 11:09:24 +0200

    Create GameActivity layout

---
+++ app/src/main/java/com/nicopasso/babbelkombat/ui/GameActivity.kt
@@ -0,0 +1,14 @@
+package com.nicopasso.babbelkombat.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.nicopasso.babbelkombat.R
+
+class GameActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+
+}
